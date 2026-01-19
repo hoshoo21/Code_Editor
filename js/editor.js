f754443db09d9a62f8288295b5f599e7bc8e03ff
hoshoo21 <c_gawera@yahoo.com>
2026-01-19 14:26:02 -0800

    editor included

--- js/editor.js
+++ js/editor.js
@@ -19,3 +19,57 @@ const escapeHTML = s=>
         }[c]
      ));
 
+function log(type="info", msg){
+    const color = type =="error" ? 'var(--err)' : type=='warning' ?'var(--warn)' : 'var(--brand)';
+    const time = new Date().toLocaleTimeString();
+    const line = document.createElement("div");
+    line.innerHTML = `<span style="color:${color}}" >[${time}] </span> ${escapeHTML(msg)} `;
+    out.appendChid(line);
+    out.scrollTop = out.scrollHeight;
+}
+
+function clearOut (){
+    out.innerHTML = "";
+}
+
+$('#clearLogs')?.addEventListener("click", clearOut());
+
+function makeEditor(id, mode){
+    const ed= ace.edit(id,{
+        'theme':'ace/theme/dracula',
+        mode : mode,
+        tabSize : 2, 
+        useSoftTabs :true ,
+        showPrintMargin : false,
+        wrap:true,
+
+    });
+    ed.session.setUserWrapMode(true);
+    ed.commands.addCommand({
+        name:"run",
+        bindKey:{
+            win:'Ctrl-Enter',
+            mac :'Command-Enter'
+        },
+        exec(){runWeb(false);}
+    });
+    ed.commands.addCommand({
+        name:"save",
+        bindKey:{
+            win:'Ctrl-S',
+            mac :'Command-S'
+        },
+        exec(){saveProject();}
+    });
+    return ed;
+
+}
+
+
+
+const ed_html =makeEditor('ed_html', 'ace/mode/html');
+const ed_css = makeEditor('ed_css', 'ace/mode/css');
+const ed_js = makeEditor('ed_js', 'ace/mode/javascript');
+
+
+
